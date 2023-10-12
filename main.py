c5f90c047324481a63bb28acf2ec2430c03f0329
Gleb <113965807+Niftysapre@users.noreply.github.com>
2023-10-12 13:43:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -46,19 +46,19 @@ def gameLoop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_RIGHT:
-                    x1_change = snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_UP:
-                    y1_change = -snake_block
-                    x1_change = 0
-                elif event.key == pygame.K_DOWN:
-                    y1_change = snake_block
-                    x1_change = 0
+       if event.type == pygame.KEYDOWN:
+             if event.key == pygame.K_LEFT and x1_change == 0:
+              x1_change = -snake_block
+              y1_change = 0
+            elif event.key == pygame.K_RIGHT and x1_change == 0:
+                x1_change = snake_block
+                 y1_change = 0
+           elif event.key == pygame.K_UP and y1_change == 0:
+                y1_change = -snake_block
+                 x1_change = 0
+          elif event.key == pygame.K_DOWN and y1_change == 0:
+                y1_change = snake_block
+               x1_change = 0
 
         if x1 >= width or x1 < 0 or y1 >= height or y1 < 0:
             game_over = True
